6682bb0cf5ccdd3056d6c687c0d1a15e0c2ca44e
ej-shafran <ejshafran@hilma.tech>
2024-11-03 10:26:56 +0200

    test: add failing test

--- packages/knex-dialect-athena/test/process.test.ts
+++ packages/knex-dialect-athena/test/process.test.ts
@@ -11,10 +11,28 @@ const client = new Client_Athena({ client: "athena" }) as unknown as {
   _processRow(row: Record<string, string | null>): Record<string, unknown>;
 };
 
+const integerProperty = it.prop([fc.integer()]);
+const nonNumberStringProperty = it.prop([
+  fc.string({ unit: fc.stringMatching(/^\D$/) }),
+]);
 const dateProperty = it.prop([fc.date()]);
 
 describe("_processRow", () => {
-  dateProperty("always processes date strings correctly", (date) => {
+  integerProperty("always processes number strings as numbers", (integer) => {
+    const integerString = integer.toString();
+    const response = client._processRow({ integerString });
+    expect(response.integerString).toBe(integer);
+  });
+
+  nonNumberStringProperty(
+    "never processes non-number strings as numbers",
+    (string) => {
+      const response = client._processRow({ string });
+      expect(response.string).not.toBeTypeOf("number");
+    },
+  );
+
+  dateProperty("never processes date strings as numbers", (date) => {
     const dateString = date.toISOString();
     const response = client._processRow({ dateString });
     expect(response.dateString).toBe(dateString);
